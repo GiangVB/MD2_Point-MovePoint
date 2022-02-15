ff3dd92411caa04e0df8e0d6bb0fc652e760702f
GiangVB <giangvb192@gmaiil.com>
2022-02-15 16:07:03 +0700

    Update

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,16 @@
 public class Test {
     public static void main(String[] args) {
         Point p1 = new Point(11, 1);
-        System.out.println(p1.toString());
+        System.out.println(p1);
 
         Point p2 = new MovablePoint(10, 12, 3, 2);
-        System.out.println(p2.toString());
+        System.out.println(p2);
         ((MovablePoint)p2).move();
-        System.out.println(p2.toString());
+        System.out.println(p2);
 
         MovablePoint m1 = new MovablePoint(1, 5, 3, 4);
-        System.out.println(m1.toString());
+        System.out.println(m1);
         m1.move();
-        System.out.println(m1.toString());
+        System.out.println(m1);
     }
 }
